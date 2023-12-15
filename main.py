78a8e555afbf0e2221365e0200088a69ead1132f
KarolisGaiv <kgaivenis@gmail.com>
2023-12-15 13:18:18 +0200

    Add init pseudo code

--- main.py
+++ main.py
@@ -1,1 +1,68 @@
 #Init main file commit
+
+
+"""
+Computer and user have the same class == Player
+
+
+class Player:
+ATTRIBUTE:
+        ******* have to have getters and setters ******
+    - stores its own deck which is provided by ???
+        
+METHOD:
+    1. draws random card
+    - if declared as winner - add both cards to the end of stack list
+    - if declared as loser - take out card from deck list attribute
+    - if declared draw - draw two cards, second drawn card is shown
+    
+    
+    
+   
+   
+   
+   
+   
+    
+class Referee:
+ATTRIBUTE:
+    - whole card deck as a list?
+
+
+METHOD:
+    1. shuffle_deck - randomly shuffles whole card deck
+    2. split_deck - give card decks to each player
+    3. declare_winner:
+        3.1 compare two cards
+        3.2 declare winner 
+"""
+
+
+
+
+
+
+
+
+""" 
+    1. start game - create both players
+    
+    2. create Referee
+        2.1 shuffle cards
+        2.2 give stacks of cards to each player
+        
+    3. computer draws card --- NO IDEA WHERE TO SAVE BOTH PLAYERS CARD. MAYBE ANOTHER class Board?????
+    4. player draws card
+    
+    5. Referee declares winner:
+        5.1  compares values of both cards
+        5.2  declares winner
+    
+    6. winner takes both cards to separate won card stack
+    
+    7. repeat step 3-6.
+
+
+
+
+"""
